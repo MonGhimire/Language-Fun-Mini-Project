bece7d7fb468aa506f0699965d8e3d5e6c95e4ad
Monika Ghimire <ghimire.monika00@gmail.com>
2022-05-24 13:29:10 +0200

    country mapping done

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -3,21 +3,61 @@ import express from "express";
 
 const apiRouter = express.Router();
 
+
+function findSubRegionCountries(data,selectedCountry){    
+    let responseToBeSent = [];
+
+     const geometriesArr = data.objects.ne_110m_admin_0_countries.geometries
+
+     const geometriesProps = geometriesArr.map((item)=>(item.properties))
+
+     const matchedCountry = geometriesProps.find((item)=>item.NAME === selectedCountry)
+     console.log(matchedCountry);
+     const subRegionCountries = (geometriesProps.filter((item)=>matchedCountry.SUBREGION === item.SUBREGION)).slice(0,4)
+     
+     const checkIfSubCountriesIncludesMatchedCountry = subRegionCountries.includes(matchedCountry)
+    //  console.log(checkIfSubCountriesIncludesMatchedCountry);
+    
+     if(!checkIfSubCountriesIncludesMatchedCountry){
+         responseToBeSent = [...subRegionCountries,matchedCountry]
+         console.log(responseToBeSent);
+    }
+    return responseToBeSent;
+}
+
+function getTranslation(countryCode,text){
+    
+
+
+
+    return "1234";
+}
+
+
+
+
 apiRouter.post("/translate", (req,res)=>{
-let arrOfCountries = [];
- console.log(req.body);
+
+//  console.log(req.body);
+
  const dataFromGeoAPI =  axios({
      url: "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json",
      method: 'get',
  }).then(result => {
-     arrOfCountries = result.data
-     const geometriesArr = arrOfCountries.objects.ne_110m_admin_0_countries.geometries
+     const countriesFromSubRegion = findSubRegionCountries(result.data,req.body.country)
+     const finalResponse = countriesFromSubRegion.map((country)=>{
+        const translatedText = getTranslation(country.ISO_A2, req.body.input)
+             
+        return {
+            "translatedText": translatedText,
+            "country": country.NAME
+        }
+    })
 
-     const geometriesProps = geometriesArr.map((item)=>(item.properties))
+     res.send(finalResponse)
 
-     const match = geometriesProps.find((item)=>item.NAME === req.body.country)
-     const subRegionCountries = geometriesProps.filter((item)=>match.SUBREGION === item.SUBREGION)
-     
+
+
     //  const firstThreeNeighbors = subRegionCountries
     //  console.log(firstThreeNeighbors);
     }).catch((error)=>console.log(error))
@@ -31,7 +71,6 @@ let arrOfCountries = [];
 
 //    const url = 'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
 
-    res.send(req.body)
 
     // console.log(responseFromAPI);
 })
